2ad1569117ad6c12634eae98cac07ca09fa23c78
Tal Yerushalmi <Razor29@gmail.com>
2023-07-13 09:52:03 +0300

    Beta version 0.9.2.3 Allow connection from any IP

--- app.py
+++ app.py
@@ -329,4 +329,4 @@ def category_total_failed_passed():
     return jsonify(report.get_total_failed_passed())
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
